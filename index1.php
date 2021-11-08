6be64aeec8c7bc38ace44cb24760062889a0a3bb
Juliya <troshina020200@icloud.com>
2021-11-08 13:48:05 +0400

    init commit

---
+++ index1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Лабораторная работа №1 </title>
+		<link rel="stylesheed" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php 
+				echo "Лабораторная работа №1 <br />";
+				echo "Вариант 2: Вычислить площадь круга <br />";
+				echo "Формула для вычисления площади круга A=pi*r*r <br />";
+				echo "r=2 <br /> pi=3.14 <br /> Площадь круга равна ";
+				echo 3.14*2*2;
+				echo "<br /> Выполнила Трошина Ю.Н. группа 20ИЭ1б";
+			?>
+		</div>
+	</body>
+</html>
